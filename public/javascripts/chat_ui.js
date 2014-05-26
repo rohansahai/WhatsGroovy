2d291253de09f5ed1190953ccbba6dbd657595ff
Rohan Sahai <rsahai91@gmail.com>
2014-05-26 16:38:19 -0700

    cleaning up unneccessary socket functions

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -8,26 +8,6 @@
   	return $("<div></div>").text(text);
   }
 
-  var updateRoomList = function(roomData){
-    $(".room-listings").empty();
-    $.each(roomData, function(room, userList){
-      if(room.length > 0){
-        var roomListing = $("<div></div>").addClass("room-listing");
-        roomListing.append($("<h3></h3>").text(room));
-        var usersUL = $("<ul></ul>");
-        $.each(userList, function(i, username){
-          usersUL.append($("<li></li>").text(username));
-        });
-        roomListing.append(usersUL);
-        $(".room-listings").append(roomListing);
-      }
-    });
-  }
-
-  var processAudio = function (chatApp) {
-    chatApp.sendAudio();
-  }
-
   var updateMousePosition = function(data) {
     //data.mouseX and mouseY are ratios of mouse position relative to canvas
     //size of other user
@@ -39,23 +19,6 @@
 
   $(document).ready(function() {
   	window.chatApp = new ChatApp.Chat(socket);
-
-    $('.send-form').submit(function(e) {
-      e.preventDefault();
-      processInput(chatApp);
-      return false;
-    });
-
-    $('#play').click(function(e) {
-      e.preventDefault();
-      processAudio(chatApp);
-      console.log('button click event');
-      return false;
-    })
-
-  	socket.on('roomList', function(roomData){
-  	  updateRoomList(roomData);
-  	});
 
     socket.on('playAudioSend', function(data){
       console.log('received audio from server');
