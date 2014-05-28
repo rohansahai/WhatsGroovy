f367576dedfbe2f544f7e3939142fe533a440bbc
Rohan Sahai <rsahai91@gmail.com>
2014-05-28 15:03:54 -0700

    got rid of hard coded host url

--- public/javascripts/audio_app.js
+++ public/javascripts/audio_app.js
@@ -41,7 +41,7 @@ $(function(){
 
         //http://localhost:8080 local hosting!
         //http://whatsgroovy.herokuapp.com  heroku hosting!
-        hostUrl = "http://localhost:8080";
+        hostUrl = window.location.origin;
         this.preLoadFiles();
         this.clicked = {};
     };
